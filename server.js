5bb5fc5d50a6d2fc637a8467903cc64f17aaf673
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-04-18 20:59:33 -0400

    404 status

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const app = express()
 
 var port = 5000
 
-const server = app.listen(port, () => )
+const server = app.listen(port, () => {
+    console.log('App is running on port %PORT%'.replace('%PORT',port))
+})
+
+app.use(funcion(req, res){
+    res.status(404).send("Endpoint does not exist")
+})
