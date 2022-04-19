2e66c546331a48649c7e7ee0c7701e9e71ec47cc
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-04-18 22:35:17 -0400

    /app update

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 })
 
-app.get('/app', (req, res) => {
-    res.status(200).end('OK')
-    res.type('text/plain')
-})
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respond with status message "OK"
+        res.statusMessage = 'OK';
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
+        res.end(res.statusCode+ ' ' +res.statusMessage)
+    });
+    
 
 app.get('/app/echo/:number', (req, res) => {
     res.status(200).json({ 'message': req.params.number })
