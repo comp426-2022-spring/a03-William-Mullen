e787ff209ab3047f5ec209f11207461edc5625ec
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-04-18 21:31:43 -0400

    status 200

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%',port))
 })
 
+app.get('/app', (req, res) => {
+    res.status(200).end('OK')
+})
+
 app.use(function(req, res) {
     res.status(404).send("Endpoint does not exist")
     res.type("text/plain")
